82a2119de60dd9e1935d0a9ff2fd7956cf326f68
Maeve Blackmore <maeveblackmore@proton.me>
2024-07-17 13:36:07 +0100

    feat(fish): add fish shell configuration

---
+++ fish/config.fish
@@ -0,0 +1,9 @@
+if status is-interactive
+    eval "$(/opt/homebrew/bin/brew shellenv)"
+    set -x STARSHIP_CONFIG ~/.config/starship/starship.toml
+    set -x GOPATH $HOME/go
+    set -x PATH $PATH $GOPATH/bin
+
+    starship init fish | source
+    zoxide init fish | source
+end
